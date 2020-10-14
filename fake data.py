85cc6c5d076272252746de45ee3a611db9feeea5
Xingzhang Zhu <xingzhangzhu@gmail.com>
2020-10-14 15:23:57 -0400

    fake data update

--- fake data.py
+++ fake data.py
@@ -1,6 +1,6 @@
 # %%
 import numpy as np
-from scipy.stats import random_correlation, multivariate_normal
+from scipy.stats import random_correlation
 
 # %%
 # specify eigenvalues
@@ -8,7 +8,8 @@ eig_v = np.random.rand(10)
 eig_v[-1] = eig_v.shape[0] - np.sum(eig_v[:-1])
 
 np.random.seed(0)
-corr_true = random_correlation.rvs(eig_v)
 mean_true = np.random.uniform(0, 0.15, 10)
 std_true = np.random.uniform(0.15, 0.5, 10).reshape(-1, 1)
-fakecov_true = std_true @ std_true.T @ corr_true
+corr_true = random_correlation.rvs(eig_v)
+cov_true = std_true @ std_true.T @ corr_true
+# %%
